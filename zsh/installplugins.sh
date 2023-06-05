f5bfb2917b8e26e62cf0a944e26eb856e2e23f22
Michael Foster <mikejoshfoster@gmail.com>
2023-06-05 09:12:10 -0700

    added p10k installation

--- zsh/installplugins.sh
+++ zsh/installplugins.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "installing powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "Done!"
+
 echo "cloning zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 echo "Done!"
@@ -8,7 +12,7 @@ echo "cloning conda-zsh-completion"
 git clone https://github.com/esc/conda-zsh-completion ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/conda-zsh-completion
 echo "Done"
 echo ""
-echo "okay, now run source .zshrc to reset the shell!"
+echo "okay, now run source .zshrc using the file in this repo to reset the shell!"
 exit 0
 
 
